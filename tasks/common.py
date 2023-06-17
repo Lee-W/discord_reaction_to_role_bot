277b7efed38c8b6a77a59e16d1b018c8a00a834f
Wei Lee <weilee.rx@gmail.com>
2023-06-17 23:54:10 +0800

    feat: project initialization

---
+++ tasks/common.py
@@ -0,0 +1,3 @@
+VENV_PREFIX = "pipenv run"
+_COMMON_TARGETS = ["bot", "tests"]
+COMMON_TARGETS_AS_STR = " ".join(_COMMON_TARGETS)
---
+++ tasks/style.py
@@ -0,0 +1,47 @@
+from invoke import task
+
+from tasks.common import COMMON_TARGETS_AS_STR, VENV_PREFIX
+
+
+@task
+def ruff(ctx):
+    """Check style through ruff"""
+    ctx.run(f"{VENV_PREFIX} ruff {COMMON_TARGETS_AS_STR}")
+
+
+@task
+def mypy(ctx):
+    """Check style through mypy"""
+    ctx.run(f"{VENV_PREFIX} mypy")
+
+
+@task
+def black_check(ctx):
+    """Check style through black"""
+    ctx.run(f"{VENV_PREFIX} black --check {COMMON_TARGETS_AS_STR}")
+
+
+@task
+def commit_check(ctx, remote="origin"):
+    """Check commit message through commitizen"""
+    ctx.run(
+        f"{VENV_PREFIX} cz -nr 3 check --rev-range {remote}/main..",
+        warn=True
+    )
+
+
+@task(pre=[ruff, mypy, black_check, commit_check], default=True)
+def run(ctx):
+    """Check style through linter (Note that pylint is not included)"""
+    pass
+
+
+@task
+def black(ctx):
+    ctx.run(f"{VENV_PREFIX} black {COMMON_TARGETS_AS_STR}")
+
+
+@task(pre=[ruff, black])
+def reformat(ctx):
+    """Reformat python files through black and isort"""
+    pass
