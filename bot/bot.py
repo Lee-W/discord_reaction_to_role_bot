33fb20de95731ac5514da866ca49780294a69352
Wei Lee <weilee.rx@gmail.com>
2023-06-17 23:58:30 +0800

    feat(bot): implement add/remove roles based on member emoji reaction

---
+++ bot/bot.py
@@ -0,0 +1,115 @@
+from __future__ import annotations
+
+import json
+import logging
+import os
+from typing import TypedDict
+
+import discord
+
+
+class Config(TypedDict):
+    discord_token: str
+    server_name: str | None
+    channel_id: int
+    add_role_message_id: int
+    remove_role_message_id: int
+    emoji_to_role_id: dict[str, int]
+
+
+class ReactionToRoleClient(discord.Client):
+    def __init__(
+        self,
+        *args,
+        channel_id: int,
+        emoji_to_role_id: dict[str, int],
+        target_guild_name: str | None = None,
+        add_role_message_id: int | None = None,
+        remove_role_message_id: int | None = None,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+
+        self.add_role_message_id = add_role_message_id
+        self.remove_role_message_id = remove_role_message_id
+        self.channel_id = channel_id
+        self.emoji_to_role_id = emoji_to_role_id
+        self.target_guild_name = target_guild_name
+
+    async def on_ready(self):
+        try:
+            # Retrieve guild object
+            if self.target_guild_name:
+                self.target_guild = list(
+                    filter(
+                        lambda guild: guild.name == self.target_guild_name, self.guilds
+                    )
+                )[0]
+            else:
+                self.target_guild = self.guilds[0]
+
+            # Retrieve channel object
+            channel = self.get_channel(self.channel_id)
+
+            # add role on emoji
+            if self.add_role_message_id:
+                await self._edit_role_on_emoji(
+                    channel, self.add_role_message_id, "add_roles"
+                )
+
+            # remove role on emoji
+            if self.remove_role_message_id:
+                await self._edit_role_on_emoji(
+                    channel, self.remove_role_message_id, "remove_roles"
+                )
+        except Exception as err:
+            logging.error(err)
+        finally:
+            await self.close()
+
+    async def _edit_role_on_emoji(
+        self,
+        channel: discord.abc.GuildChannel,
+        message_id: int,
+        operation_method_name: str,
+    ) -> None:
+        message = await channel.fetch_message(message_id)
+        for reaction in message.reactions:
+            role_id = self.emoji_to_role_id[reaction.emoji]
+            role = self.target_guild.get_role(role_id)
+            if not role:
+                # Make sure the role still exists and is valid.
+                return
+
+            async for user in reaction.users():
+                try:
+                    operation = getattr(user, operation_method_name)
+                    await operation(role)
+                except discord.HTTPException as err:
+                    logging.error(err)
+                else:
+                    await message.remove_reaction(reaction.emoji, user)
+
+
+def load_config(config_file_name: str = "config.json") -> Config:
+    raw_config_string = os.environ.get("CONFIG_JSON")
+    if not raw_config_string:
+        with open(config_file_name, "r") as input_file:
+            raw_config_string = input_file.read()
+    return json.loads(raw_config_string)
+
+
+if __name__ == "__main__":
+    config: Config = load_config()
+
+    intents = discord.Intents.default()
+    intents.members = True
+    client = ReactionToRoleClient(
+        intents=intents,
+        channel_id=config["channel_id"],
+        add_role_message_id=config["add_role_message_id"],
+        remove_role_message_id=config["remove_role_message_id"],
+        emoji_to_role_id=config["emoji_to_role_id"],
+        target_guild_name=config["server_name"],
+    )
+    client.run(config["discord_token"])
